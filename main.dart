281b6b177ec480c87ac2f8afe68fae9092d5d128
JaiAravindh-git <62341189+JaiAravindh-git@users.noreply.github.com>
2020-10-30 07:52:08 +0530

    Music Player UI
    Assets are stored locally, just a ref. file

--- main.dart
+++ main.dart
@@ -1,5 +1,3 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,64 +5,160 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  static const ktextStyle = TextStyle(fontFamily: "Work Sans", fontSize: 30.0);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-                height: 200.0,
-                width: 200.0,
-                child: Image(
-                  image: AssetImage("assets/cloud.gif"),
-                )),
-            Container(
-              child: Text(
-                'Sign In',
-                textAlign: TextAlign.left,
-                style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.w500),
-              ),
-            ),
-            SizedBox(height: 40.0),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10.0, 0, 40.0, 0),
-              child: Container(
-                child: TextField(
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.account_circle),
-                    labelText: "Username",
-                  ),
+        backgroundColor: Colors.white,
+
+        //Music Pattern
+        body: SafeArea(
+          child: Column(
+            children: [
+              //NAV BAR
+              Padding(
+                padding: const EdgeInsets.only(top: 20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Icon(Icons.arrow_back_ios_outlined),
+                    SizedBox(
+                      width: 250.0,
+                    ),
+                    Icon(Icons.list),
+                  ],
                 ),
               ),
-            ),
-            SizedBox(height: 10.0),
-            Padding(
-              padding: EdgeInsets.fromLTRB(10.0, 0, 40.0, 0),
-              child: Container(
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    icon: Icon(Icons.check_box),
-                    labelText: "Password",
+
+              //Space
+              SizedBox(
+                height: 30.0,
+              ),
+
+              //Some Random Title
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 95.0),
+                    child: Container(
+                      child: Text(
+                        "Beats that",
+                        style: ktextStyle.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                    ),
                   ),
+                  Container(
+                    child: Text(
+                      "Touch your Heart",
+                      style: ktextStyle,
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(
+                height: 40.0,
+              ),
+
+              //Images
+              Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        new BoxShadow(color: Colors.black, blurRadius: 20.0)
+                      ],
+                    ),
+                    height: 200.0,
+                    width: 200.0,
+                    child: Image.asset("assets/whenimgone.jpg"),
+                  )
+                ],
+              ),
+              //Space
+              SizedBox(
+                height: 20.0,
+              ),
+
+              //Some Random Title
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 5.0, right: 5.0),
+                    child: Container(
+                      child: Text(
+                        "When I'm Gone",
+                        style: ktextStyle.copyWith(fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      "Eminem",
+                      style: ktextStyle.copyWith(fontSize: 20.0),
+                    ),
+                  ),
+                ],
+              ),
+
+              SizedBox(
+                height: 10.0,
+              ),
+
+              Container(
+                width: 300.0,
+                child: Slider(
+                  value: 20.0,
+                  min: 0,
+                  max: 100.0,
+                  onChanged: null,
                 ),
               ),
-            ),
-            SizedBox(height: 10.0),
-            FlatButton(
-              onPressed: () {
-                null;
-              },
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(12.0)),
-              child: Text('Sign In'),
-              textColor: Colors.white,
-              color: Colors.blue,
-              splashColor: Colors.blue,
-            )
-          ],
+
+              SizedBox(
+                height: 10.0,
+              ),
+              //Player Controls
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.fast_rewind_rounded,
+                    size: 30.0,
+                  ),
+                  Icon(
+                    Icons.play_arrow_rounded,
+                    size: 30.0,
+                  ),
+                  Icon(
+                    Icons.fast_forward_rounded,
+                    size: 30.0,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.playlist_add,
+                    size: 25.0,
+                  ),
+                  SizedBox(
+                    width: 30.0,
+                  ),
+                  Icon(
+                    Icons.repeat_rounded,
+                    size: 25.0,
+                  ),
+                ],
+              )
+            ],
+          ),
         ),
       ),
       debugShowCheckedModeBanner: false,
